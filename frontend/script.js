90ffad420a792a129e8f64c1f6a3518fe1babc1f
Juan Manuel VM <juan.vargas.munoz@correounivalle.edu.co>
2025-03-30 20:17:02 -0500

    service9

--- frontend/script.js
+++ frontend/script.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         { name: "Service 6", url: "http://localhost:5006/api" },
         { name: "Service 7", url: "http://localhost:5007/api" },
         { name: "Service 8", url: "http://localhost:5008/api" },
+        { name: "Service 9", url: "http://localhost:5009/api" },
         
     ];
 
